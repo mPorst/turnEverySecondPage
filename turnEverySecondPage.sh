5f6dbd03930d525dec84361a439232cff74a53e5
Moritz <moritz.porst@gmx.de>
2019-07-06 00:06:09 +0200

    Proper variable referencing

--- turnEverySecondPage.sh
+++ turnEverySecondPage.sh
@@ -12,7 +12,7 @@ filename=$1
 parity=$2
 
 #Check if a file was given
-if [ -z "$filename" ]
+if [ -z "${filename}" ]
 then
 	echo "Please give a pdf file name"
 	echo "input format is script <filename> <parity>"
@@ -27,10 +27,10 @@ then
 	exit 1
 fi
 
-pdfSize=$($pdfinfo $1 | /usr/bin/grep Pages | $awk '{print $2}')
-if [ -z $pdfSize ]
+pdfSize=$(${pdfinfo} $1 | /usr/bin/grep Pages | $awk '{print $2}')
+if [ -z ${pdfSize} ]
 then
-	echo "The file you supplied to this humble script seems to be no pdf. Please check again if $filename is the right file"
+	echo "The file you supplied to this humble script seems to be no pdf. Please check again if ${filename} is the right file"
 	exit 1
 fi
 
@@ -39,12 +39,12 @@ echo "Assembly of your delicate pdf is in progress..."
 
 # judging from the parity, which sites are to be turned around (ty to Simon)
 # shuffle is specifically for this use case...
-if [ "$parity" == "odd" ]
+if [ "${parity}" == "odd" ]
 then
-	$pdftk $filename shuffle 1-endodddown 1-endeven output $filename"_corrected.pdf"
+	$pdftk $filename shuffle 1-endodddown 1-endeven output "${filename}_corrected.pdf"
 else
-	$pdftk $filename shuffle 1-endodd 1-endevendown output $filename"_corrected.pdf"
+	$pdftk $filename shuffle 1-endodd 1-endevendown output "${filename}_corrected.pdf"
 fi
 
 # put a kind notice there that everything is done...
-echo "Done. You may find the humble result being called by the name $filename"_corrected.pdf""  
+echo "Done. You may find the humble result being called by the name "${filename}_corrected.pdf""  
